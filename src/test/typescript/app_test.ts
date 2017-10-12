11e429e11419d51e88613dfd52a479deeb9109bc
sxend <arimitsu.k+github@gmail.com>
2017-10-13 00:06:58 +0900

    refactor config

--- src/test/typescript/app_test.ts
+++ src/test/typescript/app_test.ts
@@ -1,10 +1,6 @@
 import test from 'ava';
 import {App} from '../../main/typescript/app';
 
-test('app', t => {
-	t.pass();
-});
-
-test('bar', async function (t) {
+test('App.getMessage', async function (t) {
 	t.is(await App.getMessage(), 'message');
 });
